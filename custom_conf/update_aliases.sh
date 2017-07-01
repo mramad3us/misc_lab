5c58adb69021393d9c59287dcca72f2a93f67c28
mramad3us <mramad3us@gmail.com>
2017-07-02 01:37:13 +0200

    updating

--- custom_conf/update_aliases.sh
+++ custom_conf/update_aliases.sh
@@ -2,11 +2,12 @@
 
 # UPLOAD NEW VERSION OF ALIAS FILE TO GIT #
 # NEEDS TO BE RUN BY MAIN(NOT ROOT) USER WITH VERY SPECIFIC FS ORGANIZATION #
+# ACTUALLY, PUSH A BUNCH OF OTHER FILES IN THERE TOO... #
 
 BASEDIR=$HOME
 
 
 cp $BASEDIR/.aliases $BASEDIR/misc_lab/custom_conf
 cp $BASEDIR/.bashrc $BASEDIR/misc_lab/custom_conf
-cd $BASEDIR/misc_lab && git add custom_conf && git commit -m updating && git push
+cd $BASEDIR/misc_lab && git add custom_conf scripts && git commit -m updating && git push
 
---
+++ scripts/restore_wifi.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+systemctl start wpa_supplicant
+sudo NetworkManager
+
