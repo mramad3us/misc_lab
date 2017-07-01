e1c47c81987751ec74d3bc817f9070d4014f2ba8
mramad3us <mramad3us@gmail.com>
2017-07-01 12:43:59 +0200

    updating

--- update_aliases.sh
+++ update_aliases.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-BASEDIR=/home/mramad3us
-
 # UPLOAD NEW VERSION OF ALIAS FILE TO GIT #
 
+BASEDIR=/home/mramad3us
+
+
 cp $BASEDIR/.aliases $BASEDIR/misc_lab
-cd $BASEDIR/misc_lab && git commit -m updating && git push
+cd $BASEDIR/misc_lab && git add .aliases && git add update_aliases.sh && git commit -m updating && git push
 
