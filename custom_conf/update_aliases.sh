dcca2e7d510e5431b1fcca74d1816f2288e73877
mramad3us <mramad3us@gmail.com>
2017-07-01 12:55:51 +0200

    updating

--- custom_conf/update_aliases.sh
+++ custom_conf/update_aliases.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # UPLOAD NEW VERSION OF ALIAS FILE TO GIT #
+# NEEDS TO BE RUN BY MAIN(NOT ROOT) USER WITH VERY SPECIFIC FS ORGANIZATION #
 
-BASEDIR=/home/mramad3us
+BASEDIR=$HOME
 
 
 cp $BASEDIR/.aliases $BASEDIR/misc_lab/custom_conf
+cp $BASEDIR/.bashrc $BASEDIR/misc_lab/custom_conf
 cd $BASEDIR/misc_lab && git add custom_conf && git commit -m updating && git push
 
