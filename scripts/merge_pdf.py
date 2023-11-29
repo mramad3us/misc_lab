06cc3c672491dddbd63a8340d91185ba5a565fc1
Sam <mramad3us@gmail.com>
2023-11-29 15:29:28 +0100

    Update merge_pdf.py

--- scripts/merge_pdf.py
+++ scripts/merge_pdf.py
@@ -6,10 +6,13 @@ def merge_pdfs(front_pdf, back_pdf, output_pdf):
     reader_back = PdfReader(back_pdf)
     writer = PdfWriter()
 
+    # Reverse the order of pages in back_pdf
+    back_pages = list(reversed(reader_back.pages))
+
     # Assuming both PDFs have the same number of pages
     for i in range(len(reader_front.pages)):
         writer.add_page(reader_front.pages[i]) # Add front page
-        writer.add_page(reader_back.pages[i])  # Add back page
+        writer.add_page(back_pages[i])         # Add back page in reversed order
 
     # Write to the output PDF
     with open(output_pdf, 'wb') as output_file:
