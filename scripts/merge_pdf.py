12858daa1b65d560ceb5498a5b6343922aa3cfee
Sam <mramad3us@gmail.com>
2023-11-29 15:25:10 +0100

    Update merge_pdf.py

--- scripts/merge_pdf.py
+++ scripts/merge_pdf.py
@@ -1,20 +1,19 @@
-import PyPDF2
+from PyPDF2 import PdfReader, PdfWriter
 
 def merge_pdfs(front_pdf, back_pdf, output_pdf):
     # Open both PDF files
-    with open(front_pdf, 'rb') as file_front, open(back_pdf, 'rb') as file_back:
-        reader_front = PyPDF2.PdfFileReader(file_front)
-        reader_back = PyPDF2.PdfFileReader(file_back)
-        writer = PyPDF2.PdfFileWriter()
+    reader_front = PdfReader(front_pdf)
+    reader_back = PdfReader(back_pdf)
+    writer = PdfWriter()
 
-        # Assuming both PDFs have the same number of pages
-        for i in range(reader_front.getNumPages()):
-            writer.addPage(reader_front.getPage(i)) # Add front page
-            writer.addPage(reader_back.getPage(i))  # Add back page
+    # Assuming both PDFs have the same number of pages
+    for i in range(len(reader_front.pages)):
+        writer.add_page(reader_front.pages[i]) # Add front page
+        writer.add_page(reader_back.pages[i])  # Add back page
 
-        # Write to the output PDF
-        with open(output_pdf, 'wb') as output_file:
-            writer.write(output_file)
+    # Write to the output PDF
+    with open(output_pdf, 'wb') as output_file:
+        writer.write(output_file)
 
 # Usage
 merge_pdfs('A.pdf', 'B.pdf', 'Merged.pdf')
