80a1595f51e279d842d98638f503bd948503bf44
Sam <mramad3us@gmail.com>
2023-12-16 22:38:02 +0100

    Update get_of_v2.py

--- planning_reader/test/get_of_v2.py
+++ planning_reader/test/get_of_v2.py
@@ -1,12 +1,3 @@
-From inspecting the 'POINTAGE' sheet, it's evident that:
-
-1. **Column 'Unnamed: 1'**: Contains project or affair names, such as 'LP41 S04', 'AL08'.
-2. **Column 'Unnamed: 2' (labeled 'INTERNE')**: Likely contains the 'INTERNE' OF codes corresponding to the projects.
-3. **Column 'Unnamed: 3' (labeled 'EXTERNE')**: Likely contains the 'EXTERNE' OF codes corresponding to the projects.
-
-Based on this, we can adjust the script to use 'Unnamed: 1' for project codes and 'Unnamed: 2' and 'Unnamed: 3' for 'INTERNE' and 'EXTERNE' OF codes, respectively. Here's the revised script:
-
-```python
 import pandas as pd
 
 def read_planning_data(file_path):
@@ -16,18 +7,20 @@ def read_planning_data(file_path):
     return planning_df, pointage_df
 
 def get_of_code_mappings(pointage_df):
-    # Map project codes to their 'INTERNE' and 'EXTERNE' OF codes
+    # Map project codes to their 'INTERNE' and 'EXTERNE' OF codes, trimming spaces
     of_code_mappings = {}
     for _, row in pointage_df.iterrows():
         project_code = row['Unnamed: 1']
         if pd.notna(project_code):
+            # Remove any spaces to match the project code format
+            project_code = ''.join(project_code.split())
             of_code_mappings[project_code] = {
                 'INTERNE': row['Unnamed: 2'],
                 'EXTERNE': row['Unnamed: 3']
             }
     return of_code_mappings
 
-# Rest of the script remains the same...
+# The rest of the script remains the same
 
 def main():
     file_path = 'path_to_your_excel_file.xlsx'  # Replace with your actual file path
@@ -45,6 +38,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-```
-
-This script now uses the corrected column names from the 'POINTAGE' sheet to map project codes to their 'INTERNE' and 'EXTERNE' OF codes. Please ensure to replace `'path_to_your_excel_file.xlsx'` with the actual path to your Excel file.
