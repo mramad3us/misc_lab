7df67b2b238e427d83cff2443a33a90f94877a83
Sam <mramad3us@gmail.com>
2023-12-16 22:55:07 +0100

    Update get_of_v2.py

--- planning_reader/test/get_of_v2.py
+++ planning_reader/test/get_of_v2.py
@@ -1,40 +1,69 @@
 import pandas as pd
 
 def read_planning_data(file_path):
-    # Load the 'ANNEE 2023' sheet and the 'POINTAGE' sheet
     planning_df = pd.read_excel(file_path, sheet_name='ANNEE 2023')
     pointage_df = pd.read_excel(file_path, sheet_name='POINTAGE')
     return planning_df, pointage_df
 
 def get_of_code_mappings(pointage_df):
-    # Map project codes to their 'INTERNE' and 'EXTERNE' OF codes, trimming spaces
     of_code_mappings = {}
     for _, row in pointage_df.iterrows():
         project_code = row['Unnamed: 1']
         if pd.notna(project_code):
-            # Remove any spaces to match the project code format
-            project_code = ''.join(project_code.split())
+            project_code = ''.join(project_code.split())  # Remove spaces
             of_code_mappings[project_code] = {
                 'INTERNE': row['Unnamed: 2'],
                 'EXTERNE': row['Unnamed: 3']
             }
     return of_code_mappings
 
-# The rest of the script remains the same
+def extract_week_data(planning_df, week_number, of_code_mappings):
+    start_col_index = 273 + (week_number - 39) * 7
+    end_col_index = start_col_index + 6
+    samuel_murez_row_range = (546, 550)
+
+    week_data = planning_df.iloc[samuel_murez_row_range[0]:samuel_murez_row_range[1], start_col_index:end_col_index]
+
+    project_info = {}
+    for col in week_data.columns:
+        for row in week_data[col].dropna().unique():
+            if ' ' in row:
+                project_code, customer_name = row.split(' ', 1)
+                of_codes = of_code_mappings.get(project_code, {'INTERNE': None, 'EXTERNE': None})
+                project_info[project_code] = {
+                    'Customer Name': customer_name,
+                    'OF Codes': of_codes
+                }
+    return project_info
+
+def read_timesheet_template(file_path):
+    timesheet_df = pd.read_excel(file_path, sheet_name='Your_Timesheet_Sheet_Name')  # Replace with your sheet name
+    return timesheet_df
+
+def prefill_of_numbers(timesheet_df, project_info):
+    for project_code, details in project_info.items():
+        row_index = timesheet_df[timesheet_df['Project Code Column'] == project_code].index
+        if row_index.empty:
+            continue
+        of_code = details['OF Codes']['INTERNE'] or details['OF Codes']['EXTERNE']
+        timesheet_df.at[row_index, 'OF Number Column'] = of_code  # Replace 'OF Number Column'
+    return timesheet_df
 
 def main():
-    file_path = 'Planning SAV.xlsx'  # Replace with your actual file path
-    planning_df, pointage_df = read_planning_data(file_path)
+    planning_file_path = 'Planning SAV.xlsx'  # Replace with your file path
+    timesheet_file_path = 'Feuille_pointage_.xlsx'  # Replace with your file path
 
-    # Get the OF code mappings from the 'POINTAGE' sheet
+    planning_df, pointage_df = read_planning_data(planning_file_path)
     of_code_mappings = get_of_code_mappings(pointage_df)
 
-    # Prompt user for a week number
     week_number = int(input("Enter the week number: "))
-
-    # Extract and display project information
     project_info = extract_week_data(planning_df, week_number, of_code_mappings)
-    print("Project Codes, Customer Names, and OF Codes for Week", week_number, ":", project_info)
+
+    timesheet_df = read_timesheet_template(timesheet_file_path)
+    updated_timesheet_df = prefill_of_numbers(timesheet_df, project_info)
+
+    updated_timesheet_df.to_excel('updated_timesheet.xlsx', index=False)
+    print("Updated timesheet saved to 'updated_timesheet.xlsx'.")
 
 if __name__ == "__main__":
     main()
